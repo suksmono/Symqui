40cce9931229760e811a798c0957e78ad1cd8564
suksmono <38452529+suksmono@users.noreply.github.com>
2019-05-04 08:22:58 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='symqui',
+    url='https://github.com/suksmono/symque',
+    author='Andriyan Suksmono',
+    author_email='suksmono@yahoo.com',
+    #install_requires=['sympy'],
+    version='0.1dev',
+    packages=['symqui'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
